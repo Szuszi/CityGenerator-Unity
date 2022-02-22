2ddf2d7fc89cc2ef54bd5091516b9353ce3ffd05
Álmos Koncz <koncz.almos13@gmail.com>
2022-02-22 10:54:02 +0100

    Add GizmoService for cleaner code

--- CityGenerator2D/Assets/Scripts/CityGenerator.cs
+++ CityGenerator2D/Assets/Scripts/CityGenerator.cs
@@ -14,10 +14,12 @@ namespace Assets.Scripts
         private List<Lot> Lots;
         private System.Random rand;
 
+        private GizmoService gizmoService; //For drawing with gizmos
+
         private List<Lot> ConcaveLots;
         private List<Lot> ConvexLots;
         private List<LotMesh> LotMeshes;
-        private float LotHeight = 0.1f;
+        private float LotHeight = 0.02f;
 
         [Header("Maximum Curve between Roads")]
         [Range(2, 20)]
@@ -57,6 +59,7 @@ namespace Assets.Scripts
         {
             rand = new System.Random(seed);
             roadGraph = new Graph();
+            gizmoService = new GizmoService();
             Thread t = new Thread(new ThreadStart(ThreadProc));
             t.Start();
         }
@@ -79,109 +82,37 @@ namespace Assets.Scripts
 
             if (drawRoads)
             {
-                DrawEdges(roadGraph.MajorEdges, Color.white);
-                DrawEdges(roadGraph.MinorEdges, Color.black);
+                gizmoService.DrawEdges(roadGraph.MajorEdges, Color.white);
+                gizmoService.DrawEdges(roadGraph.MinorEdges, Color.black);
             }
 
             if (drawRoadNodes)
             {
-                DrawNodes(roadGraph.MajorNodes, Color.white, 0.2f);
-                DrawNodes(roadGraph.MinorNodes, Color.black, 0.1f);
+                gizmoService.DrawNodes(roadGraph.MajorNodes, Color.white, 0.2f);
+                gizmoService.DrawNodes(roadGraph.MinorNodes, Color.black, 0.1f);
             }
 
             if (drawLotNodes)
             {
-                DrawLotNodes(LotNodes, Color.red, 0.04f);
+                gizmoService.DrawLotNodes(LotNodes, Color.red, 0.04f);
             }
 
             if (drawLots)
             {
-                DrawLots(Lots, new Color(0.7f, 0.4f, 0.4f));
+                gizmoService.DrawLots(Lots, new Color(0.7f, 0.4f, 0.4f));
             }
 
             if (drawConvexLots)
             {
-                DrawLots(ConvexLots, new Color(0.2f, 0.7f, 0.7f));
+                gizmoService.DrawLots(ConvexLots, new Color(0.2f, 0.7f, 0.7f));
             }
             if(drawConcaveLots)
             {
-                DrawLots(ConcaveLots, new Color(0.2f, 0.7f, 0.2f));
+                gizmoService.DrawLots(ConcaveLots, new Color(0.2f, 0.7f, 0.2f));
             }
             if (drawTriangulatedMeshes)
             {
-                DrawLotMeshes(LotMeshes, new Color(.8f, .8f, .8f));
-            }
-        }
-
-        private void DrawNodes(List<Node> nodes, Color color, float size)
-        {
-            for (int x = nodes.Count - 1; x > -1; x--) //for loop start from backwards, because the list is getting new elements while beeing read
-            {
-                Gizmos.color = color;
-                Gizmos.DrawSphere(new Vector3(nodes[x].X, nodes[x].Y, 0f), size);
-            }
-        }
-
-        private void DrawLotNodes(List<LotNode> nodes, Color color, float size)
-        {
-            if (LotNodes == null) return;
-
-            Gizmos.color = color;
-            for (int x = nodes.Count - 1; x > -1; x--) //for loop start from backwards, because the list is getting new elements while beeing read
-            {
-                Gizmos.DrawSphere(new Vector3(nodes[x].X, nodes[x].Y, 0f), size);
-            }
-        }
-
-        private void DrawLots(List<Lot> Lots, Color color)
-        {
-            if (Lots == null) return;
-
-            Gizmos.color = color;
-            foreach(Lot lot in Lots)
-            {
-                for(int i = 0; i < lot.Nodes.Count; i++)
-                {
-                    if(i == (lot.Nodes.Count - 1))
-                    {
-                        Vector3 from = new Vector3(lot.Nodes[i].X, lot.Nodes[i].Y, 0f);
-                        Vector3 to = new Vector3(lot.Nodes[0].X, lot.Nodes[0].Y, 0f);
-                        Gizmos.DrawLine(from, to);
-                    }
-                    else
-                    {
-                        Vector3 from = new Vector3(lot.Nodes[i].X, lot.Nodes[i].Y, 0f);
-                        Vector3 to = new Vector3(lot.Nodes[i + 1].X, lot.Nodes[i + 1].Y, 0f);
-                        Gizmos.DrawLine(from, to);
-                    }
-                }
-            }
-        }
-
-        private void DrawLotMeshes(List<LotMesh> lotMeshes, Color color)
-        {
-            if (lotMeshes == null) return;
-
-            Gizmos.color = color;
-            foreach(LotMesh lotMesh in lotMeshes)
-            {
-                foreach(Triangle tri in lotMesh.triangles)
-                {
-                    Gizmos.DrawLine(tri.A, tri.B);
-                    Gizmos.DrawLine(tri.A, tri.C);
-                    Gizmos.DrawLine(tri.B, tri.C);
-                }
-            }
-        }
-
-        private void DrawEdges(List<Edge> edges, Color color)
-        {
-            for (int x = edges.Count - 1; x > -1; x--) //for loop start from backwards, because the list is getting new elements while beeing read
-            {
-                Gizmos.color = color;
-                Vector3 from = new Vector3(edges[x].NodeA.X, edges[x].NodeA.Y, 0f);
-                Vector3 to = new Vector3(edges[x].NodeB.X, edges[x].NodeB.Y, 0f);
-                Gizmos.DrawLine(from, to);
+                gizmoService.DrawLotMeshes(LotMeshes, new Color(.8f, .8f, .8f));
             }
         }
 
---
+++ CityGenerator2D/Assets/Scripts/GizmoService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    class GizmoService
+    {
+        public void DrawNodes(List<Node> nodes, Color color, float size)
+        {
+            for (int x = nodes.Count - 1; x > -1; x--) //for loop start from backwards, because the list is getting new elements while beeing read
+            {
+                Gizmos.color = color;
+                Gizmos.DrawSphere(new Vector3(nodes[x].X, nodes[x].Y, 0f), size);
+            }
+        }
+
+        public void DrawLotNodes(List<LotNode> nodes, Color color, float size)
+        {
+            if (nodes == null) return;
+
+            Gizmos.color = color;
+            for (int x = nodes.Count - 1; x > -1; x--) //for loop start from backwards, because the list is getting new elements while beeing read
+            {
+                Gizmos.DrawSphere(new Vector3(nodes[x].X, nodes[x].Y, 0f), size);
+            }
+        }
+
+        public void DrawLots(List<Lot> Lots, Color color)
+        {
+            if (Lots == null) return;
+
+            Gizmos.color = color;
+            foreach (Lot lot in Lots)
+            {
+                for (int i = 0; i < lot.Nodes.Count; i++)
+                {
+                    if (i == (lot.Nodes.Count - 1))
+                    {
+                        Vector3 from = new Vector3(lot.Nodes[i].X, lot.Nodes[i].Y, 0f);
+                        Vector3 to = new Vector3(lot.Nodes[0].X, lot.Nodes[0].Y, 0f);
+                        Gizmos.DrawLine(from, to);
+                    }
+                    else
+                    {
+                        Vector3 from = new Vector3(lot.Nodes[i].X, lot.Nodes[i].Y, 0f);
+                        Vector3 to = new Vector3(lot.Nodes[i + 1].X, lot.Nodes[i + 1].Y, 0f);
+                        Gizmos.DrawLine(from, to);
+                    }
+                }
+            }
+        }
+
+        public void DrawLotMeshes(List<LotMesh> lotMeshes, Color color)
+        {
+            if (lotMeshes == null) return;
+
+            Gizmos.color = color;
+            foreach (LotMesh lotMesh in lotMeshes)
+            {
+                foreach (Triangle tri in lotMesh.triangles)
+                {
+                    Gizmos.DrawLine(tri.A, tri.B);
+                    Gizmos.DrawLine(tri.A, tri.C);
+                    Gizmos.DrawLine(tri.B, tri.C);
+                }
+            }
+        }
+
+        public void DrawEdges(List<Edge> edges, Color color)
+        {
+            for (int x = edges.Count - 1; x > -1; x--) //for loop start from backwards, because the list is getting new elements while beeing read
+            {
+                Gizmos.color = color;
+                Vector3 from = new Vector3(edges[x].NodeA.X, edges[x].NodeA.Y, 0f);
+                Vector3 to = new Vector3(edges[x].NodeB.X, edges[x].NodeB.Y, 0f);
+                Gizmos.DrawLine(from, to);
+            }
+        }
+    }
+}
