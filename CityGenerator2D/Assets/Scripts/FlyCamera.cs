42b57c5f50fe63bb82620e471265fc4fdec515d7
Álmos Koncz <koncz.almos13@gmail.com>
2022-04-10 22:10:49 +0200

    Add FlyCamera script

---
+++ CityGenerator2D/Assets/Scripts/FlyCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+public class FlyCamera : MonoBehaviour
+{
+    private Vector3 _angles;
+    public float speed = 1.0f;
+    public float fastSpeed = 2.0f;
+    public float mouseSpeed = 4.0f;
+ 
+    private void OnEnable() {
+        _angles = transform.eulerAngles;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+ 
+    private void OnDisable() { Cursor.lockState = CursorLockMode.None; }
+ 
+    private void Update() {
+        _angles.x -= Input.GetAxis("Mouse Y") * mouseSpeed;
+        _angles.y += Input.GetAxis("Mouse X") * mouseSpeed;
+        transform.eulerAngles = _angles;
+        float moveSpeed = Input.GetKey(KeyCode.LeftShift) ? fastSpeed : speed;
+        transform.position +=
+            Input.GetAxis("Horizontal") * moveSpeed * transform.right +
+            Input.GetAxis("Vertical") * moveSpeed * transform.forward;
+    }
+}
